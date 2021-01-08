5828cd1545939b78898016bc6b438eec1a785df7
Jeffrey Tan <jeffreytan@Jeffreys-MacBook-Air.local>
2021-01-09 01:08:49 +0800

    Enhance UI

--- soul.py
+++ soul.py
@@ -22,58 +22,71 @@ def getIdx(n1, n2):
 
 def getNextState(i, j, num):
 	currIdx = getIdx(i,j)
-	currNum = (num | 1 << currIdx) 
 	res = []
 
+	#check if curr dot selected
 	if (num & (1 << currIdx)):
 		return res
 
+	#select current row and col (i, j)
+	currNum = (num | 1 << currIdx) 
 	res.append(currNum)
 
+
 	for k in range(len(dr)):
+		#backup currNum
 		bak = currNum
+
 		for m in range(1, nMove):
 			currI = i + (m * dr[k])
 			currJ = j + (m * dc[k])
 
+			#Unreachable dot
 			if currI < 0 or currJ < 0 or currJ > currI or currI >= nRow:
 				break
 
 			idx = getIdx(currI, currJ)
+
+			#check if dot is selected
 			if not (currNum & (1 << idx)):
 				currNum |= (1 << idx)
 				res.append(currNum)
 			else:
 				break
+
 		currNum = bak
 	return res
 
 
 def generateState(num):
 	activeBit = countActiveBit(num)
-	if activeBit == nDot - 1: 
-		return 0
-	if activeBit >= nDot:
-		return 1
+
+	# return winning or losing state (base case)
+	if activeBit >= nDot - 1:
+		return (activeBit != nDot - 1)
 
 	if dp[num] != -1:
 		return dp[num]
 
-	winState = 0;
-	winMove = -1;
+	winState =  0;
+	winMove  = -1;
+
 	for i in range(nRow):
 		for j in range(i + 1):
 			nextState = getNextState(i, j, num)
+
 			for state in nextState:
 				isWin = not generateState(state)
 				winState |= isWin
+
 				if winMove == -1:
 					winMove = state;
 				elif(isWin == 1):
 					winMove = state
 
 	dpMove[num] = winMove 
-	dp[num] = winState 
+	dp[num]     = winState 
+	
 	return winState
 
 generateState(0)
